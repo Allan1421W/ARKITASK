4481251ede9f9fe61c44a84a6e6aeb8af341045c
Allan1421W <allansamirb@gmail.com>
2023-05-07 14:52:21 -0500

    Menus casi terminados

--- Static/main.js
+++ Static/main.js
@@ -2,7 +2,6 @@ $('.sections article').hide();
 $('.sections #tab1').show();
 $('ul.tabs li a').click(function(){
     $('.sections article').hide();
-
     var activeTab = $(this).attr('href');
     $(activeTab).show();     
     return false;
