f90bea0ba6f1af63f0912f53ceb1b022d3721144
Allan1421W <allansamirb@gmail.com>
2023-06-11 17:26:46 -0500

    CRUD gerente

--- Static/JavaScript/app.js
+++ Static/JavaScript/app.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   };
 
   firebase.initializeApp(firebaseConfig);
-
+// REGISTRAR USUARIO <----
 //Elementos para abrir el boton de registrar usuario
 const openModal = document.getElementById('openRegister');
 const modal = document.getElementById('modal');
@@ -24,9 +24,10 @@ const registerForm = document.getElementById('register-form');
 const studentRef = firebase.database().ref('Usuarios');
 const studentData = document.getElementById('studentsTable');
 
-const showRegisterModal = () => (
+
+const showRegisterModal = () => {
     modal.classList.toggle('is-active')
-)
+}
 
 openModal.addEventListener('click', (showRegisterModal))
 closeModal.addEventListener('click', (showRegisterModal))
@@ -42,10 +43,10 @@ const showUpdateModal = () => {
 
 closeUpdateModal.addEventListener('click', showUpdateModal)
 
-window.addEventListener('DOMContentLoaded', async (e) => {
-    await studentRef.on('value', (students) => {
+window.addEventListener('DOMContentLoaded', async (a) => {
+    await studentRef.on('value', (users) => {
         studentsTable.innerHTML = ''
-        students.forEach((student) =>{
+        users.forEach((student) =>{
             let studentData = student.val()
             studentsTable.innerHTML += `
             <tr>
@@ -66,19 +67,19 @@ window.addEventListener('DOMContentLoaded', async (e) => {
             `
             const updateButtons = document.querySelectorAll('.is-warning')
             updateButtons.forEach((button) => {
-                button.addEventListener('click', (e) => {
+                button.addEventListener('click', (a) => {
                     showUpdateModal()
-                    firebase.database().ref(`Usuarios/${e.target.dataset.id}`).once('value').then((student) =>{
+                    firebase.database().ref(`Usuarios/${a.target.dataset.id}`).once('value').then((student) =>{
                         const data = student.val()
                         updateForm['nombre'].value = data.Nombre
                         updateForm['apePri'].value = data.PrimerApe
                         updateForm['apeSeg'].value = data.SegunApe
-                        updateForm['cel'].value = data.Telefono
+                             updateForm['cel'].value = data.Telefono
                         updateForm['email'].value = data.Correo
                         updateForm['descrip'].value = data.Descripcion
                     })
-                    const uid = e.target.dataset.id
-                    updateForm.addEventListener('submit', (e) => {
+                    const uid = a.target.dataset.id
+                    updateForm.addEventListener('submit', (a) => {
                         e.preventDefault()
 
                         const nombre = updateForm['nombre'].value
@@ -103,7 +104,7 @@ window.addEventListener('DOMContentLoaded', async (e) => {
 
             const deleteButtons = document.querySelectorAll('.is-danger')
             deleteButtons.forEach((button) => {
-                button.addEventListener('click', (e) => {
+                button.addEventListener('click', (a) => {
                     deleteStudent(e.target.dataset.id)
                 })
             })
@@ -111,8 +112,8 @@ window.addEventListener('DOMContentLoaded', async (e) => {
     })
 })
 
-registerForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+registerForm.addEventListener('submit', (a) => {
+    a.preventDefault()
     
     const nombre = registerForm['nombre'].value
     const apellidoPri = registerForm['apePri'].value
@@ -132,4 +133,217 @@ registerForm.addEventListener('submit', (e) => {
         Descripcion: descripcion,
     })
     showRegisterModal()
+});
+
+// REGISTRAR PROYECTO <----
+//Elementos para abrir el boton de registrar usuario
+const openModalPro = document.getElementById('openProyect');
+const modalPro = document.getElementById('modalProyect');
+const closeModalPro = document.getElementById('closeProyectModal');
+
+const proyectForm = document.getElementById('proyect-form');
+const proyectRef = firebase.database().ref('Proyectos');
+const proyectsTable = document.getElementById('proyectTable');
+
+
+const showProyectModal = () => {
+    modalPro.classList.toggle('is-active')
+}
+
+openModalPro.addEventListener('click', (showProyectModal))
+closeModalPro.addEventListener('click', (showProyectModal))
+
+window.addEventListener('DOMContentLoaded', async (b) => {
+    await proyectRef.on('value', (proyects) => {
+        proyects.forEach(proyect => {
+            const proyectData = proyect.val()
+            proyectsTable.innerHTML += `<tr>
+            <td>${proyectData.NombrePro}</td>
+            <td>${proyectData.UidPro}</td>
+            <td>${proyectData.FechaEntregaPro}</td>
+            <td>${proyectData.FechaRealizacionPro}</td>
+            <td>${proyectData.EstadoPro}</td>
+          </tr>
+            `
+        });
+    })
 })
+
+proyectForm.addEventListener('submit', (b) => {
+    b.preventDefault()
+    
+    const nombrePro = proyectForm['nombrePro'].value
+    const fechEntrPro = proyectForm['fechEntrPro'].value
+    const fechReaPro = proyectForm['fechReaPro'].value
+    const estaPro = proyectForm['estaPro'].value
+
+    const registerProyect = proyectRef.push()
+    registerProyect.set({
+        UidPro: registerProyect._delegate._path.pieces_[1],
+        NombrePro: nombrePro,
+        FechaEntregaPro: fechEntrPro,
+        FechaRealizacionPro: fechReaPro,
+        EstadoPro: estaPro,
+    })
+    showProyectModal()
+});
+
+//REGISTRAR ASISTENCIA <----
+const openModalAsis = document.getElementById('openAsistencia');
+const modalAsis = document.getElementById('modalAsistencia');
+const closeModalAsis = document.getElementById('closeAsistenciaModal');
+
+const asistenciaForm = document.getElementById('asistencia-form');
+const asistenciaRef = firebase.database().ref('Asistencia');
+const asistenciaTable = document.getElementById('asistenciaTable');
+
+
+const showAsistenciaModal = () => {
+    modalAsis.classList.toggle('is-active')
+}
+
+openModalAsis.addEventListener('click', (showAsistenciaModal))
+closeModalAsis.addEventListener('click', (showAsistenciaModal))
+
+window.addEventListener('DOMContentLoaded', async (c) => {
+    await asistenciaRef.on('value', (asistencias) => {
+        asistencias.forEach(asistencia => {
+            const asistenciaData = asistencia.val()
+            asistenciaTable.innerHTML += `<tr>
+            <td>${asistenciaData.NombreAsis}</td>
+            <td>${asistenciaData.FechaIngresoAsis}</td>
+            <td>${asistenciaData.HoraIngresoAsis}</td>
+            <td>${asistenciaData.FechaSalidaAsis}</td>
+            <td>${asistenciaData.HoraSalidaAsis}</td>
+          </tr>
+            `
+        });
+    })
+})
+
+asistenciaForm.addEventListener('submit', (c) => {
+    c.preventDefault()
+    
+    const nombreAsis = asistenciaForm['nombreAsis'].value
+    const fechaIngAsis = asistenciaForm['fechaIngAsis'].value
+    const horaIngAsis = asistenciaForm['horaIngAsis'].value
+    const fechaSalAsis = asistenciaForm['fechaSalAsis'].value
+    const horaSalAsis = asistenciaForm['horaSalAsis'].value
+
+    const registerAsistencia = asistenciaRef.push()
+    registerAsistencia.set({
+        UidAsis: registerAsistencia._delegate._path.pieces_[1],
+        NombreAsis: nombreAsis,
+        FechaIngresoAsis: fechaIngAsis,
+        HoraIngresoAsis: horaIngAsis,
+        FechaSalidaAsis: fechaSalAsis,
+        HoraSalidaAsis: horaSalAsis,
+    })
+    showAsistenciaModal()
+});
+
+//REGISTRAR GRUPO DE PROYECTO <----
+const openModalGroup = document.getElementById('openGroup');
+const modalGroup = document.getElementById('modalGroup');
+const closeModalGroup = document.getElementById('closeGroupModal');
+
+const groupForm = document.getElementById('group-form');
+const groupRef = firebase.database().ref('GruposProyectos');
+const groupTable = document.getElementById('groupTable');
+
+
+const showGroupModal = () => {
+    modalGroup.classList.toggle('is-active')
+}
+
+openModalGroup.addEventListener('click', (showGroupModal))
+closeModalGroup.addEventListener('click', (showGroupModal))
+
+window.addEventListener('DOMContentLoaded', async (d) => {
+    await groupRef.on('value', (groups) => {
+        groups.forEach(group => {
+            const groupData = group.val()
+            groupTable.innerHTML += `<tr>
+            <td>${groupData.UidGroup}</td>
+            <td>${groupData.FechaIniGroup}</td>
+            <td>${groupData.FechaDesGroup}</td>
+            <td>${groupData.CantidadGroup}</td>
+          </tr>
+            `
+        });
+    })
+})
+
+groupForm.addEventListener('submit', (d) => {
+    d.preventDefault()
+    
+    const fechaIniGroup = groupForm['fechaIniGroup'].value
+    const fechaDesGroup = groupForm['fechaDesGroup'].value
+    const canGroup = groupForm['canGroup'].value
+
+    const registerGroup = groupRef.push()
+    registerGroup.set({
+        UidGroup: registerGroup._delegate._path.pieces_[1],
+        FechaIniGroup: fechaIniGroup,
+        FechaDesGroup: fechaDesGroup,
+        CantidadGroup: canGroup,
+    })
+    showGroupModal()
+});
+
+// REGISTRAR ENTREGABLES <----
+const openModalEntre = document.getElementById('openEntre');
+const modalEntre = document.getElementById('modalEntre');
+const closeModalEntre = document.getElementById('closeEntreModal');
+
+const entregableForm = document.getElementById('entregable-form');
+const entregableRef = firebase.database().ref('Entregables');
+const entregablesTable = document.getElementById('entregablesTable');
+
+
+const showEntregableModal = () => {
+    modalEntre.classList.toggle('is-active')
+}
+
+openModalEntre.addEventListener('click', (showEntregableModal))
+closeModalEntre.addEventListener('click', (showEntregableModal))
+
+window.addEventListener('DOMContentLoaded', async (f) => {
+    await entregableRef.on('value', (entregables) => {
+        entregables.forEach(entregable => {
+            const entregableData = entregable.val()
+            entregablesTable.innerHTML += `<tr>
+            <td>${entregableData.NombreEntre}</td>
+            <td>${entregableData.FechaEntre}</td>
+            <td>${entregableData.DescripcionEntre}</td>
+            <td>${entregableData.ImagenEntre}</td>
+            <td>${entregableData.ComenEntre}</td>
+            <td>${entregableData.EstadoEntre}</td>
+          </tr>
+            `
+        });
+    })
+})
+
+entregableForm.addEventListener('submit', (f) => {
+    f.preventDefault()
+    
+    const nombreEntre = entregableForm['nombreEntre'].value
+    const fechaEntre = entregableForm['fechaEntre'].value
+    const descripEntre = entregableForm['descripEntre'].value
+    const imagenEntre = entregableForm['imagenEntre'].value
+    const comenEntre = entregableForm['comenEntre'].value
+    const estadoEntre = entregableForm['estadoEntre'].value
+
+    const registerEntre = entregableRef.push()
+    registerEntre.set({
+        UidEntre: registerEntre._delegate._path.pieces_[1],
+        NombreEntre: nombreEntre,
+        FechaEntre: fechaEntre,
+        DescripcionEntre: descripEntre,
+        ImagenEntre: imagenEntre,
+        ComenEntre: comenEntre,
+        EstadoEntre: estadoEntre,
+    })
+    showEntregableModal()
+});
